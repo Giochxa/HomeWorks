5eeca03476fd39faac50a079842e742490b43989
Giochxa <gio.chxaidze@hotmail.com>
2023-12-13 04:07:42 +0400

    Week 4 First push

---
+++ Homework 2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Homework_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine(Console.ReadLine());
+            //Console.WriteLine("Giorgi Chkhaidze");
+        }
+    }
+}
