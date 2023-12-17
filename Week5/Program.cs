e5aebc4003cb63647d5f7a67f8940447331cbc19
Giochxa <gio.chxaidze@hotmail.com>
2023-12-18 01:02:25 +0400

    Week 6 First push

--- Week5/Program.cs
+++ Week5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Week5
 {
@@ -61,7 +62,8 @@ namespace Week5
             //var y = Convert.ToDecimal(Console.ReadLine());
             //Console.WriteLine("x = " + x + "     y = " + y);
             //x = x + y;
-            //y = x - y; x = x - y;
+            //y = x - y;
+            //x = x - y;
             //Console.WriteLine("x = " + x + "     y = " + y);
             #endregion
 
@@ -76,16 +78,81 @@ namespace Week5
             #endregion
 
             #region Task5
-            Console.WriteLine("Enter X");
-            var x = Convert.ToDecimal(Console.ReadLine());
-            for (int i = 1; i < x; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(i*i);
-                }
-            }
+            //Console.WriteLine("Enter X");
+            //var x = Convert.ToDecimal(Console.ReadLine());
+            //for (int i = 1; i < x; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i*i);
+            //    }
+            //}
             #endregion
+
+            #region Week 6 task
+            //decimal [] arr = new decimal[5];
+            //decimal a;
+            //decimal sum=0;
+            //Console.WriteLine("Enter Array numbers of 5");
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    a = Convert.ToDecimal(Console.ReadLine());
+            //    arr[i]= a*a;
+            //    sum += arr[i]; 
+            //}
+            //Console.WriteLine("Elements in the Array are ");
+            //foreach (var item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine($"Sum of elements is {sum}");
+
+            #endregion
+            #region Week6 task 2
+            //Console.WriteLine("Enter Word");
+            //var word = Console.ReadLine();
+            //int symbolCount = word.Count();
+            //Console.WriteLine(symbolCount);
+
+            //---------------2-------------------
+            //Console.Write("Enter a string: ");
+            //string userInput = Console.ReadLine();
+
+            //// Count the occurrences of each symbol in the string
+            //var symbolCounts = userInput
+            //    .GroupBy(c => c)
+            //    .OrderBy(g => g.Key);
+
+            //// Display the results
+            //Console.WriteLine("Symbol Counts:");
+            //foreach (var group in symbolCounts)
+            //{
+            //    Console.WriteLine($"{group.Key}: {group.Count()} times");
+            //}
+
+
+            //------------------3----------------------
+            //Console.WriteLine("Enter a string:");
+            //string input = Console.ReadLine();
+
+            //int count = input.Select(ch => 1).Sum();
+            //Console.WriteLine(count);
+
+            #endregion
+            #region week6 task 3
+            string[] cities = new string[] { "Tbilisi", "Rome", "Gori", "Abasha", "Telavi" };
+            Console.WriteLine("Enter the starting character city name starts with: ");
+            char start = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Enter the starting character city ends starts with: ");
+            char end = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine($"Entered start {start} and end {end} cities are: ");
+            var match = cities.Where(str => str.StartsWith(start) && str.EndsWith(end));
+            foreach (var item in match)
+            {
+                Console.WriteLine(item);
+            }
+            
+           #endregion
         }
     }
 }
---
+++ Week6/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Week6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+            ////დაწერეთ პროგრამა რომელიც კონსოლიდან მიიღებს მასივის ზომას , n
+            ////რაოდენობის ელემენტს შეინახავს მასივში. შემდეგ ამ მასივიდან გაფილტრავს
+            ////ელემენტს ლუწ რიცხვებს შეინახავს 1 მასივში ხოლო კენტებს მეორეში .
+            //Console.WriteLine("Enter the length of array: ");
+            //int size = Convert.ToInt32(Console.ReadLine());
+            //int [] array = new int[size];
+            //int[] oddArray=new int[size];
+            //int[] evenArray = new int[size];
+            //var evenCount = 0;
+            //var oddCount = 0;
+            //Console.WriteLine($"Enter {size} values for the array:");
+            //for (int i = 0; i < size; i++) 
+            //{
+
+            //    array[i] = int.Parse(Console.ReadLine());
+            //    if (array[i] % 2 == 0)
+            //    {
+            //        oddArray[evenCount] = array[i];
+            //        evenCount++;
+            //    }
+            //    else
+            //    {
+            //        evenArray[oddCount] = array[i];
+            //        oddCount++;
+            //    }
+            //}
+            //Console.WriteLine("Original Array: ");
+            //foreach (int i in array)
+            //    Console.WriteLine(i + " ");
+            //Console.WriteLine("Odd Array:");
+            //foreach (int i in oddArray)
+            //    Console.WriteLine(i + " ");
+            //Console.WriteLine("Even Array: ");
+            //foreach (int i in evenArray)
+            //    Console.WriteLine(i + " ");
+            #endregion
+
+            #region Task 2
+            //დაწერეთ პროგრამა რომელიც მასივში დათვლის თითოეული ელემენტის რაოდენობას და გამოიტანს მათ ჯამს.
+            //Console.WriteLine("Enter the length of array: ");
+            //int size = Convert.ToInt32(Console.ReadLine());
+            //int[] array = new int[size];
+            //Console.WriteLine($"Enter {size} values for the array:");
+            //for (int i = 0; i < size; i++)
+            //{
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+            //// Count the occurrences of each symbol in the string
+            //var symbolCounts = array
+            //    .GroupBy(c => c)
+            //    .OrderBy(g => g.Key);
+
+            //// Display the results
+            //Console.WriteLine("Symbol Counts:");
+            //foreach (var group in symbolCounts)
+            //{
+            //    Console.WriteLine($"{group.Key} appears {group.Count()} times sum {group.Sum()}");
+            //}
+            #endregion
+
+            #region Task 3
+            //დაწერეთ პროგრამა რომელიც გვიჩვენებს ტოპ n მონაწილის შედეგს . მაგ(1 2 3 4 5 6 7 8 9 10)
+            Console.WriteLine("Enter the Number of Participants: ");
+            int number = int.Parse(Console.ReadLine());
+            int[] results = new int[number];
+            Console.WriteLine($"Enter {number} results of the Participants:");
+            for (int i = 0; i < number; i++)
+            {
+                results[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Enter how many top results to display:");
+            var amount = int.Parse(Console.ReadLine());
+            foreach (var item in results.OrderBy(g => g).TakeLast(amount))
+            {
+                Console.WriteLine(item);
+            }
+            #endregion
+        }
+    }
+}
