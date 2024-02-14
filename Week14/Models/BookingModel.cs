886be90b2c5876f540c5aa007c9109fffc935fc3
Giochxa <gio.chxaidze@hotmail.com>
2024-02-14 10:59:49 +0400

    Week 14 push

---
+++ Week14/Models/BookingModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using System;
+
+namespace Week14.Models
+{
+    public class BookingModel
+    {
+        //public int PersonId { get; set; }
+        //public string Name { get; set; }
+        //public string Gender { get; set; }
+        //public string City { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Doctor { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
